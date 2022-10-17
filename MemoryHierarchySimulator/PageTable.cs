92a1a4f061f43eb55ce4ddc406eb87a61a051a14
dayjn1 <janinenday@gmail.com>
2022-10-17 01:09:02 -0400

    Basic setup for page table & PTE

--- MemoryHierarchySimulator/PageTable.cs
+++ MemoryHierarchySimulator/PageTable.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace MemoryHierarchySimulator
 {
-    internal class PageTable
+    // set up as a linear PT
+    // OSTEP Notes:
+    // The simplest form is called a linear page table, which is just an array.
+    // The OS indexes the array by the virtual page number(VPN), and looks up
+    // the page-table entry(PTE) at that index in order to find the desired physical frame number(PFN)
+
+    public class PageTable
     {
+        PageTableEntry[] PT;
+
+        public PageTable(int NumVirtPages, int NumPhysPages, int PageSize)
+        {
+            PT = new PageTableEntry[NumVirtPages];
+
+            // need to figure out conversion of VPN to PFN
+        }
+
+
     }
 }
---
+++ MemoryHierarchySimulator/PageTableEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryHierarchySimulator
+{
+    public class PageTableEntry
+    {
+        int PFN;                    // page frame number
+        int PresentAbsentBit;       // present - 1, absent - 0
+        int ProtectionBit;          // read, write, etc
+        int ReferencedBit;          // set to 1 when page is accessed
+        int CachingEnabledDisabled; // enables or diables caching
+        int ModifiedBit;            // dirty bit, set to 1 on write
+
+        // the above was pulled from https://www.geeksforgeeks.org/page-table-entries-in-page-table/
+        // so they might not be necessary for our purposes
+
+        public PageTableEntry()
+        {
+
+        }
+    }
+}
