91b645055ecdbc7b652a1962f9644e9987bb1948
dayjn1 <janinenday@gmail.com>
2022-10-17 16:50:00 -0400

    Update PTE

--- MemoryHierarchySimulator/PageTableEntry.cs
+++ MemoryHierarchySimulator/PageTableEntry.cs
@@ -9,14 +9,11 @@ namespace MemoryHierarchySimulator
     public class PageTableEntry
     {
         int PFN;                    // page frame number
-        int PresentAbsentBit;       // present - 1, absent - 0
-        int ProtectionBit;          // read, write, etc
-        int ReferencedBit;          // set to 1 when page is accessed
-        int CachingEnabledDisabled; // enables or diables caching
-        int ModifiedBit;            // dirty bit, set to 1 on write
-
-        // the above was pulled from https://www.geeksforgeeks.org/page-table-entries-in-page-table/
-        // so they might not be necessary for our purposes
+        int ValidBit;               // is translation in PTE valid
+        int ProtectionBits;         // read, write, etc
+        int PresentBit;             // present - 1, absent - 0
+        int DirtyBit;               // page modified since load
+        int AccessedBit;            // set to 1 when page is accessed
 
         public PageTableEntry()
         {
