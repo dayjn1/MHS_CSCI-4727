6a0828f643e4f20a6dd8cd510f9151aac573d3e3
FarmerNB <farmernb@etsu.edu>
2022-10-20 21:01:06 -0400

    basic skeleton of TLB

--- MemoryHierarchySimulator/DTLB.cs
+++ MemoryHierarchySimulator/DTLB.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,37 @@ namespace MemoryHierarchySimulator
 {
     internal class DTLB
     {
-    }
+        public byte[] TLB;
+        public int numOfLines;
+
+        //configuration
+        private int numOfSets { get; set; }
+        private int setSize { get; set; }
+        private int numOfBitsForIndex { get; set; }
+
+        public DTLB(byte virtAddress)
+        {
+            numOfSets = int.Parse(ConfigurationManager.AppSettings.Get("TLB Number of sets"));
+
+            setSize = int.Parse(ConfigurationManager.AppSettings.Get("TLB Set size"));
+
+            numOfLines = numOfSets * setSize;
+
+            TLB = new byte[numOfLines];
+
+        }
+
+        public TlbHit findInTlb()
+        {
+            return 0;
+        }
+
+        public void clearCache()
+        {
+            
+        }
+    }   
+
+
+
 }
