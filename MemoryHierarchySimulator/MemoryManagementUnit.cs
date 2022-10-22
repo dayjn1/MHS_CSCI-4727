678c83197517c072f4a7a11343da8480ae7aa8cd
Carlos Ortiz <ortizc@etsu.edu>
2022-10-22 11:02:18 -0400

    Created a method that will parse the .dat files to get the address from them

--- MemoryHierarchySimulator/MemoryManagementUnit.cs
+++ MemoryHierarchySimulator/MemoryManagementUnit.cs
@@ -12,12 +12,28 @@ namespace MemoryHierarchySimulator
     {
         static public void Main(String[] args)
         {
-            DisplayConfigSettings();
-
-            //Cache example
+            //Will hold the addresses from the inputted files
+            string[] addressLines;
+            string[] address;
             DataCache dc = new DataCache();
 
-            dc.findCacheVariables(123);
+            //int that will hold the addresses int
+            int virtAddress;
+            DisplayConfigSettings();
+            addressLines = ParseInputFiles();
+
+            //runs through each address and sends it through TLB, Page Table, and Cache
+            for(int x = 0; x < addressLines.Length; x++)
+            {
+                address = addressLines[x].Split(":");
+                //address[0] is the read or write char
+
+                //address[1] is the address itself
+                virtAddress = Convert.ToInt32(address[1], 16);
+                dc.findCacheVariables(virtAddress);
+            }
+            //Cache example
+
             CacheHit ch = dc.findInstructionInCache();
             dc.updateCacheTag();
 
@@ -64,5 +80,28 @@ namespace MemoryHierarchySimulator
                 Console.WriteLine("The addresses read in are physical addresses.");
 
         }
+
+        /// <summary>Gets all the addresses from an inputted file</summary>
+        /// <returns>String Array with addresses</returns>
+        public static string[] ParseInputFiles()
+        {
+            string fileName = "";
+            string[] lines;
+            Console.WriteLine("Enter the file name you would like to open (real_tr.dat/trace.dat)");
+            fileName = Console.ReadLine();
+            //Gets the size and coordinates from Test.txt 
+            try
+            {
+                string path = Path.GetFullPath(fileName);     //Gets the path to Test.txt
+                lines = System.IO.File.ReadAllLines(path);
+                return lines;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("File does not exist!");
+                return null;
+            }
+        }
+
     }
 }
